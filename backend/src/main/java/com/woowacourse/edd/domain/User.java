49ffd864c073ccc5a2f2cc1e06cb660fcfadb6cb
MK S <conatuseus@gmail.com>
2019-08-16 16:33:15 +0900

    [feat] fix Username to name

--- backend/src/main/java/com/woowacourse/edd/domain/User.java
+++ backend/src/main/java/com/woowacourse/edd/domain/User.java
@@ -1,8 +1,8 @@
 package com.woowacourse.edd.domain;
 
 import com.woowacourse.edd.domain.vo.Email;
+import com.woowacourse.edd.domain.vo.Name;
 import com.woowacourse.edd.domain.vo.Password;
-import com.woowacourse.edd.domain.vo.UserName;
 
 import javax.persistence.*;
 
@@ -14,29 +14,25 @@ public class User {
     private Long id;
 
     @Embedded
-    private UserName userName;
-
-    @Embedded
     private Email email;
 
     @Embedded
     private Password password;
 
+    @Embedded
+    private Name name;
+
     private User() {
     }
 
-    public User(UserName userName, Email email, Password password) {
-        this.userName = userName;
+    public User(Email email, Password password, Name name) {
         this.email = email;
         this.password = password;
+        this.name = name;
     }
 
     public Long getId() {
         return id;
-    }
-
-    public UserName getUserName() {
-        return userName;
     }
 
     public Email getEmail() {
@@ -45,5 +41,9 @@ public class User {
 
     public Password getPassword() {
         return password;
+    }
+
+    public Name getName() {
+        return name;
     }
 }
