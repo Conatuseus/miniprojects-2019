40bcd532a53a05b4cc37c488c4f242d2b5074dda
MK S <conatuseus@gmail.com>
2019-08-22 21:20:18 +0900

    [refactor] remove wildcard import

--- backend/src/main/java/com/woowacourse/edd/domain/User.java
+++ backend/src/main/java/com/woowacourse/edd/domain/User.java
@@ -3,7 +3,11 @@ package com.woowacourse.edd.domain;
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
 
-import javax.persistence.*;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import java.time.LocalDateTime;
 
 @Entity
