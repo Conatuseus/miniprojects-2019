53baaccd33a4e25e3088aedca473dcf8a204bd6d
MK S <conatuseus@gmail.com>
2019-09-01 16:56:36 +0900

    [fix] fix message for name, email

--- backend/src/main/java/com/woowacourse/edd/exceptions/DuplicateEmailSignUpException.java
+++ backend/src/main/java/com/woowacourse/edd/exceptions/DuplicateEmailSignUpException.java
@@ -4,7 +4,7 @@ import org.springframework.http.HttpStatus;
 
 public class DuplicateEmailSignUpException extends ErrorResponseException {
 
-    public static final String DUPLICATE_EMAIL_SIGNUP_MESSAGE = "이미 존재하는 이메일입니다.";
+    public static final String DUPLICATE_EMAIL_SIGNUP_MESSAGE = "이미 존재하거나 탈퇴한 회원의 이메일은 사용하실 수 없습니다.";
 
     public DuplicateEmailSignUpException() {
         super(DUPLICATE_EMAIL_SIGNUP_MESSAGE, HttpStatus.BAD_REQUEST);
